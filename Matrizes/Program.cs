d46e896bf8663a775456bd55833c2e5ac924aff5
matheuefranco <matheusefranco@gmail.com>
2024-10-31 08:53:40 -0300

    Aula31-10

---
+++ Matrizes/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Biblioteca_Matriz;
+class Program
+{
+    static void Main()
+    {
+        int linhas, cols;
+        Console.WriteLine("**** Matrizes ****");
+        Console.WriteLine("Qtde de linhas e colunas:");
+        linhas = int.Parse(Console.ReadLine());
+        cols = int.Parse(Console.ReadLine());
+        int[,] matrix = new int[linhas, cols]; 
+        Console.WriteLine("Gerando dados para matriz:");
+        Matriz.gera(matrix);
+        Console.WriteLine("Dados da matriz:");
+        Matriz.mostra(matrix);
+        Console.ReadKey();
+    }
+}
